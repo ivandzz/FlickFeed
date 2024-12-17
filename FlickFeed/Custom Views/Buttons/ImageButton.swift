275b44e5381ddf80fe909db1984c60f22797f5b9
Ivan Dzhulynskyi <ivan.dzhul@gmail.com>
2024-12-17 22:32:36 +0200

    refactor: Small code refactor

--- FlickFeed/Custom Views/Buttons/ImageButton.swift
+++ FlickFeed/Custom Views/Buttons/ImageButton.swift
@@ -10,14 +10,14 @@ import UIKit
 class ImageButton: UIButton {
     
     //MARK: - Variables
-    var size: CGFloat
-    var imageName: String
     
     //MARK: - Lifecycle
     init(imageName: String, size: CGFloat) {
-        self.size = size
-        self.imageName = imageName
         super.init(frame: .zero)
+        
+        if let image = UIImage(systemName: imageName)?.withConfiguration(UIImage.SymbolConfiguration(pointSize: size, weight: .regular)) {
+            self.setImage(image, for: .normal)
+        }
         
         setupUI()
     }
@@ -28,9 +28,6 @@ class ImageButton: UIButton {
     
     //MARK: - UI Setup
     private func setupUI() {
-        if let image = UIImage(systemName: imageName)?.withConfiguration(UIImage.SymbolConfiguration(pointSize: size, weight: .regular)) {
-            self.setImage(image, for: .normal)
-        }
 
         self.tintColor              = .white
         self.imageView?.contentMode = .scaleAspectFit
