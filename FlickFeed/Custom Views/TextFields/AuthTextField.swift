5b3c941b246101ced837354c763ad9bec3bdc214
Ivan Dzhulynskyi <ivan.dzhul@gmail.com>
2025-01-22 20:02:12 +0200

    feat: Added show/hide password button

--- FlickFeed/Custom Views/TextFields/AuthTextField.swift
+++ FlickFeed/Custom Views/TextFields/AuthTextField.swift
@@ -16,32 +16,13 @@ class AuthTextField: UITextField {
         case loginPassword
         case registerPassword
     }
-
+    
     // MARK: - Lifecycle
     init(fieldType: AuthTextFieldType) {
         super.init(frame: .zero)
         
         setupUI()
-        
-        switch fieldType {
-        case .username:
-            self.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-            
-        case .email:
-            self.attributedPlaceholder = NSAttributedString(string: "Email Address", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-            self.keyboardType          = .emailAddress
-            self.textContentType       = .emailAddress
-            
-        case .loginPassword:
-            self.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-            self.textContentType       = .password
-            self.isSecureTextEntry     = true
-            
-        case .registerPassword:
-            self.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-            self.textContentType       = .newPassword
-            self.isSecureTextEntry     = true
-        }
+        configure(for: fieldType)
     }
     
     required init?(coder: NSCoder) {
@@ -65,5 +46,66 @@ class AuthTextField: UITextField {
             self.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.85),
             self.heightAnchor.constraint(equalToConstant: 55)
         ])
+    }
+    
+    private func configure(for fieldType: AuthTextFieldType) {
+        switch fieldType {
+        case .username:
+            setPlaceholder(with: "Username")
+        case .email:
+            setPlaceholder(with: "Email Address")
+            self.keyboardType = .emailAddress
+            self.textContentType = .emailAddress
+        case .loginPassword:
+            setPlaceholder(with: "Password")
+            self.textContentType = .password
+            self.isSecureTextEntry = true
+            setupPasswordVisibilityButton()
+        case .registerPassword:
+            setPlaceholder(with: "Password")
+            self.textContentType = .newPassword
+            self.isSecureTextEntry = true
+            setupPasswordVisibilityButton()
+        }
+    }
+    
+    private func setupPasswordVisibilityButton() {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        button.setImage(UIImage(systemName: "eye"), for: .selected)
+        button.tintColor = .lightGray
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            containerView.widthAnchor.constraint(equalToConstant: 52),
+            containerView.heightAnchor.constraint(equalToConstant: 40),
+        ])
+        
+        containerView.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
+            button.widthAnchor.constraint(equalToConstant: 40),
+            button.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        self.rightView = containerView
+        self.rightViewMode = .always
+        
+        button.addTarget(self, action: #selector(showHidePassword), for: .touchUpInside)
+    }
+    
+    // MARK: - Selectors
+    @objc private func showHidePassword(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        self.isSecureTextEntry.toggle()
+    }
+    
+    //MARK: - Helper Functions
+    private func setPlaceholder(with text: String) {
+        self.attributedPlaceholder = NSAttributedString(string: text, attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
     }
 }
